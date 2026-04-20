491738b09901fddfa3920f28ba669d474ff75696
Andro2012 <andro20123337@mail.ru>
2026-04-20 13:46:02 +0300

    Update mygame.py
    добавлено много всего(ничего)

--- mygame.py
+++ mygame.py
@@ -3,21 +3,27 @@ from PIL import Image, ImageTk
 from threading import Event, Thread
 
 R = 10
-place_x = 0
-place_y = 0
+place_x = 500
+place_y = 450
 X = 1000
 Y = 900
 register = False
 ev = Event()
 
 class Player_class():
-    def __init__(self):
+    def __init__(self, x = place_x, y = place_y):
+        self.x = x
+        self.y = y
+
+
+
         self.root = tk.Tk()
-        self.root.geometry(f"{X}x{Y}")
+        self.root.geometry(f"{X}x{Y}+200+50")
+        self.root.resizable(False, False)
 
         player_image = Image.open('myphoto.png') #65x99
-        player_tk = ImageTk.PhotoImage(player_image)
-        self.player = tk.Label(image=player_tk)
+        player_im_tk = ImageTk.PhotoImage(player_image)
+        self.player = tk.Label(image=player_im_tk)
         self.player.place(x = place_x, y = place_y)
 
 
@@ -28,36 +34,45 @@ class Player_class():
         self.root.bind('<d>', self.move_right)
         self.root.bind('<D>', self.move_right)
 
-        self.root.bind('<Escape>', Player.exit) #выход из игры
+        self.root.bind('<space>', self.jump)
 
-    def move_left(self):
-        pass
+        self.root.bind('<Escape>', self.exit) #выход из игры
+
+    def move_left(self, event):
+        self.x -= R
+
+
+        print('left')
     
-    def move_right(self):
-        pass
+    def move_right(self, event):
+        print('right')
 
-    def jump(self):
-        pass
+    def jump(self,event):
+        print('jump')
 
-    def myfunc_e(event):
-        print('В разработке...')
-        print(event)  
+    def place(self, event):
+        self.player.place(x = place_x, y = place_y)
 
-    def exit(self):
+    
+
+    def myfunc_e(self, event):
+        print('Инвентарь в разработке...')
+
+    def exit(self, event):
         self.root.destroy()
         print('Игра закрыта')
         
 
 class Settings():
-    def __init__(self):
+    def __init__(self, admin_panel = False):
         self.settings = Player_class()
 
     def start(self):
         self.settings.root.mainloop()
 
 
-main_settings = Settings()
-main_settings.start()
 
 
-
+if __name__ == "__main__":
+    main_settings = Settings()
+    main_settings.start()
