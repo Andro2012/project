8ef9fa70e9539bc03dd7f8adae3b0614053ebac4
Andro2012 <andro20123337@mail.ru>
2026-05-06 13:46:56 +0300

    Update mygame.py

--- mygame.py
+++ mygame.py
@@ -15,15 +15,16 @@ class Player_class():
         self.x = x
         self.y = y
 
-
+        self.is_jumping = False
 
         self.root = tk.Tk()
+        self.canvas = tk.Canvas(self.root, width = 500, height = 400)
         self.root.geometry(f"{X}x{Y}+200+50")
         self.root.resizable(False, False)
 
-        player_image = Image.open('myphoto.png') #65x99
-        player_im_tk = ImageTk.PhotoImage(player_image)
-        self.player = tk.Label(image=player_im_tk)
+        self.player_image = Image.open('myphoto.png') #65x99
+        self.player_im_tk = ImageTk.PhotoImage(self.player_image)
+        self.player = tk.Label(image = self.player_im_tk)
         self.player.place(x = place_x, y = place_y)
 
 
@@ -38,27 +39,78 @@ class Player_class():
 
         self.root.bind('<Escape>', self.exit) #выход из игры
 
-    def move_left(self, event):
-        self.x -= R
-
+    def move_left(self, event = None):
+        if self.x <= R:
+            self.x = 0
+            self.place()
+        else:
+            self.x -= R
+            self.place()
 
         print('left')
     
-    def move_right(self, event):
+    def move_right(self, event = None):
+        if self.x >= X - R:
+            self.x = X - R
+            self.place()
+        else:
+            self.x += R
+            self.place()
+
+
         print('right')
 
-    def jump(self,event):
-        print('jump')
+    def jump(self,event = None):
 
-    def place(self, event):
-        self.player.place(x = place_x, y = place_y)
+        if self.is_jumping:
+            return
+        
+        else:
+            self.jump_frames = 40
+
+            print("UP")
+            for i in range(self.jump_frames):
+                self.root.after(100, self.jump_up)
+                
+            print("FALL")
+            for i in range(self.jump_frames):
+                self.root.after(100, self.jump_falling)
+
+            print('jump')
+
+    def jump_up(self, event = None):
+        if self.is_jumping:
+            return
+        
+        else:
+            if self.jump_frames > 0:
+                self.y -= 1
+                self.place()
+                self.root.after(20, self.jump_up)
+                self.jump_frames -= 1
+            else:
+                self.jump_frames = 40
+                self.jump_falling()
+
+    def jump_falling(self, event = None):
+        if self.jump_frames > 0:
+            self.y += 1
+            self.place()
+            self.root.after(20, self.jump_falling)
+            self.jump_frames -= 1
+        else:
+            self.is_jumping = False
+
+
+    def place(self, event = None):
+        self.player.place(x = self.x, y = self.y)
 
     
 
-    def myfunc_e(self, event):
+    def myfunc_e(self, event = None):
         print('Инвентарь в разработке...')
 
-    def exit(self, event):
+    def exit(self, event = None):
         self.root.destroy()
         print('Игра закрыта')
         
